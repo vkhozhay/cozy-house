9cc44b2aee32c75ecade3656a5816b66b9dabd52
Vlad Khozhay <vladyslav.khozhai@hneu.net>
2020-01-13 17:46:32 +0200

    4 commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@
  let navButton = document.querySelector('#navigation-mobile__icon');
  let mobileNavigation = document.querySelector('#navigation-mobile');
  let sectionArr = document.querySelectorAll('section');
+ let navMobileItems = document.querySelectorAll('.navigation-mobile__item');
+
+ for (i = 0; i < navMobileItems.length; i++) {
+     navMobileItems[i].onclick = function () {
+         navButton.classList.toggle('navigation-mobile__icon-active');
+         mobileNavigation.classList.toggle('navigation-mobile_active');
+
+         for (i = 0; i < sectionArr.length; i++) {
+             sectionArr[i].classList.toggle('section__menu-active');
+         }
+     }
+ }
 
 
 
@@ -16,12 +28,29 @@
 
  }
 
+ $('.flowing-scroll').on('click', function () {
+     var el = $(this);
+     var dest = el.attr('href'); // получаем направление
+     if (dest !== undefined && dest !== '') { // проверяем существование
+         $('html').animate({
+                 scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+             }, 500 // скорость прокрутки
+         );
+     }
+     return false;
+ });
+
 
  var swiper = new Swiper('.swiper-container', {
      slidesPerView: 3,
      spaceBetween: 50,
      slidesPerGroup: 1,
+     speed: 500,
      loop: true,
+
+     autoplay: {
+         delay: 4000,
+     },
 
      navigation: {
          nextEl: '.slider-button-next',
